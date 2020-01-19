62d87a879ab0c3043b19e0eda35c67da070c7a04
stevemeister5 <stephenreubenm@gmail.com>
2020-01-19 15:10:46 +0300

    Build UI and Custom post writer

--- src/client/App.js
+++ src/client/App.js
@@ -1,6 +1,9 @@
 /* eslint-disable react/prefer-stateless-function */
 import React, { Component } from 'react';
 
+// Bundling our app with CSS by calling the import statement
+import '../../assets/css/style.css';
+
 const posts = [{
   id: 2,
   text: 'Lorem Ipsum',
@@ -19,14 +22,47 @@ const posts = [{
 }];
 
 export default class App extends Component {
+
   state = {
-    posts: posts
+    posts: posts,
+    postContent: ''
   }
+
+  handlePostContentChange = (event) => {
+    this.setState({postContent: event.target.value})
+  }
+
+  handleSubmit = (event) => {
+    // Prevent default to stop the page from reloading
+    event.preventDefault();
+    // Creating the new post
+    const newPost = {
+      id: this.state.posts.length + 1,
+      text: this.state.postContent,
+      user: {
+        avatar: '/uploads/avatar1.png',
+        username: 'Fake User'
+      }
+    };
+    // Updating the posts
+    this.setState((prevState) => ({
+      posts: [newPost, ...prevState.posts],
+      postContent: ''
+    }));
+  }
+
   render() {
-    const { posts } = this.state;
+
+    const { posts, postContent } = this.state;
 
     return (
       <div className="container">
+        <div className="postForm">
+          <form onSubmit={this.handleSubmit}>
+            <textarea value={postContent} onChange={this.handlePostContentChange} placeholder="Write your custom post!"/>
+            <input type="submit" value="Submit"/>
+          </form>
+        </div>
         <div className="feed">
           {posts.map((post, i) =>
             <div key={post.id} className="post">
