5764df10555eb3f90fe3d7e5884bc9d6de662ef8
stevemeister5 <stephenreubenm@gmail.com>
2020-01-20 09:42:15 +0300

    Initial Apoolo and Graphql setup

--- src/server/index.js
+++ src/server/index.js
@@ -4,6 +4,9 @@ import helmet from 'helmet';
 import cors from 'cors';
 import compress from 'compression';
 
+// Import our Services Object
+import services from './services';
+
 
 const app = express();
 
@@ -28,10 +31,23 @@ app.use(cors());
 
 app.use(helmet.referrerPolicy({ policy: 'same-origin' }));
 
+
 app.use('/', express.static(path.join(root, 'dist/client')));
 app.use('/uploads', express.static(path.join(root, 'uploads')));
 app.get('/', (req, res) => {
   res.sendFile(path.join(root, 'dist/client/index.html'));
 });
+
+// Binding Graphql to express web server
+const serviceNames = Object.keys(services);
+
+for (let i = 0; i < serviceNames.length; i += 1) {
+  const name = serviceNames[i];
+  if (name === 'graphql') {
+    services[name].applyMiddleware({ app });
+  } else {
+    app.use(`/${name}`, services[name]);
+  }
+}
 
 app.listen(8000, () => console.log('Listening on port 8000!'));
