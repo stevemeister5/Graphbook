12a75851f9d15cfbe21e9da5dd211c1cb41b76bc
stevemeister5 <stephenreubenm@gmail.com>
2020-01-20 09:29:26 +0300

    Finish Express Middleware setup

--- src/server/index.js
+++ src/server/index.js
@@ -1,10 +1,32 @@
 import express from 'express';
 import path from 'path';
+import helmet from 'helmet';
+import cors from 'cors';
+import compress from 'compression';
 
 
 const app = express();
 
 const root = path.join(__dirname, '../../');
+
+// Compression setup
+app.use(compress());
+
+// Helmet Middleware setup
+app.use(helmet());
+app.use(helmet.contentSecurityPolicy({
+  directives: {
+    defaultSrc: ["'self'"],
+    scriptSrc: ["'self'", "'unsafe-inline'"],
+    styleSrc: ["'self'", "'unsafe-inline'"],
+    imgSrc: ["'self'", "data:", "*.amazonaws.com"]
+  }
+}));
+
+// CORS setup
+app.use(cors());
+
+app.use(helmet.referrerPolicy({ policy: 'same-origin' }));
 
 app.use('/', express.static(path.join(root, 'dist/client')));
 app.use('/uploads', express.static(path.join(root, 'uploads')));
